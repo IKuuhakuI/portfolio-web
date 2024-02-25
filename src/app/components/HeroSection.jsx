0a5d5f9512db0af382bae060900b2916c95524e6
luiz.carrion <luiz.carrion@ip.tv>
2024-02-25 03:44:05 -0300

    add hero section elements

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
@@ -21,6 +22,54 @@ const HeroSection = () => {
             suscipit ligula. Ut ligula est, aliquam a fermentum ac, pharetra vel
             est. Nam sed viverra ipsum. In tempus porta diam ac faucibus.
           </p>
+          <div>
+            <button
+              className="
+                px-6 
+                py-3 
+                rounded-full 
+                mr-4 
+                bg-white 
+                hover:bg-slate-300 
+                text-black
+              "
+            >
+              Hire Me
+            </button>
+            <button
+              className="
+                px-6 
+                py-3 
+                rounded-full 
+                bg-transparent 
+                hover:bg-slate-800 
+                text-white border 
+                border-white mt-3"
+            >
+              Download CV
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div
+            className="
+              rounded-full 
+              bg-[#181818] 
+              w-[250px] 
+              h-[250px] 
+              lg:w-[400px] 
+              lg:h-[400px] 
+              relative
+            "
+          >
+            <Image
+              src="/images/hero_image.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
       </div>
     </section>
