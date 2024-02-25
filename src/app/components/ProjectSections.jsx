faab81f8db1514a5cedfca44675a3a197722ccca
luiz.carrion <luiz.carrion@ip.tv>
2024-02-25 09:48:05 -0300

    Create ProjectCard component

---
+++ src/app/components/ProjectSections.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Infinity Worldle",
+    description: "[ADD DESCRIPTION]",
+    image: "/images/projects/infinity_worldle.png",
+    tag: ["All", "Games", "JS"],
+  },
+  {
+    id: 2,
+    title: "Fullstack Go Web",
+    description: "[ADD DESCRIPTION]",
+    image: "/images/projects/fullstack_go.png",
+    tag: ["All", "Frontend", "Backend", "JS", "GO"],
+  },
+  {
+    id: 3,
+    title: "StockWise",
+    description: "[ADD DESCRIPTION]",
+    image: "/images/projects/stockwise.jpeg",
+    tag: ["All", "Frontend", "Backend", "TypeScript"],
+  },
+  {
+    id: 4,
+    title: "ZiulFlix",
+    description: "[ADD DESCRIPTION]",
+    image: "/images/projects/ziul_flix.jpeg",
+    tag: ["All", "Frontend", "Backend", "JS"],
+  },
+  {
+    id: 5,
+    title: "Python Web Scraping",
+    description: "[ADD DESCRIPTION]",
+    image: "/images/projects/web-scraping-with-python.png",
+    tag: ["All", "Python"],
+  },
+  {
+    id: 6,
+    title: "All Heroes Must Die",
+    description: "[ADD DESCRIPTION]",
+    image: "/images/projects/all_heroes_must_die.png",
+    tag: ["All", "Games", "C#"],
+  },
+  {
+    id: 7,
+    title: "Portfolio Web",
+    description: "[ADD DESCRIPTION]",
+    image: "/images/projects/portfolio.png",
+    tag: ["All", "Frontend", "JS"],
+  },
+  {
+    id: 8,
+    title: "Pong4Math",
+    description: "[ADD DESCRIPTION]",
+    image: "/images/projects/pong_4_math.png",
+    tag: ["All", "Games", "JS"],
+  },
+  {
+    id: 9,
+    title: "Infinity Questions",
+    description: "[ADD DESCRIPTION]",
+    image: "/images/projects/infinity_questions.png",
+    tag: ["All", "Games", "Python"],
+  },
+];
+
+const ProjectSections = () => {
+  return (
+    <>
+      <h2>MyProjects</h2>
+      <div></div>
+    </>
+  );
+};
+
+export default ProjectSections;
