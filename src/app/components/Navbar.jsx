4c4c9f2714fd46f18b45759cf96e4fc23e7017d2
luiz.carrion <luiz.carrion@ip.tv>
2024-02-25 04:34:43 -0300

    Make NavLink a component

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,5 +1,21 @@
 import Link from "next/link";
 import React from "react";
+import NavLink from "./NavLink";
+
+const navLinks = [
+  {
+    title: "About",
+    path: "#about",
+  },
+  {
+    title: "Projects",
+    path: "#projects",
+  },
+  {
+    title: "Contact",
+    path: "#contact",
+  },
+];
 
 const Navbar = () => {
   return (
@@ -10,24 +26,11 @@ const Navbar = () => {
         </Link>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul>
-            <li>
-              <Link
-                href={"#about"}
-                className="
-                  block 
-                  py-2 
-                  pl-3 
-                  pr-4 
-                  text-[#ADB7BE] 
-                  sm:text-xl 
-                  rounded 
-                  md:p-0 
-                  hover:text-white
-                "
-              >
-                About
-              </Link>
-            </li>
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
           </ul>
         </div>
       </div>
